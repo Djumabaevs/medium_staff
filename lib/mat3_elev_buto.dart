5793fd5cb004003401008cd0c953b75980e7ecfc
Djumabaevs <djumabaevb@gmail.com>
2023-03-12 16:55:03 +0600

    main

--- lib/mat3_elev_buto.dart
+++ lib/mat3_elev_buto.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'KindaCode.com',
+        title: 'KindaCodeMain.com',
         theme: ThemeData(
           useMaterial3: true, // enable Material 3
           primarySwatch: Colors.blue,
