4e11be3ed3bff137cfa80c848aed8fc6996f2db8
Djumabaevs <djumabaevb@gmail.com>
2023-05-15 00:43:40 +0600

    no but

--- lib/riverpod_form_validation/ex_fire.dart
+++
@@ -1,9 +0,0 @@
-@Riverpod(keepAlive: true)
-AuthRepository authRepository(AuthRepositoryRef ref) {
-  return AuthRepository(ref.watch(firebaseAuthProvider));
-}
-
-@riverpod
-Stream<User?> authStateChanges(AuthStateChangesRef ref) {
-  return ref.watch(authRepositoryProvider).authStateChanges();
-}
